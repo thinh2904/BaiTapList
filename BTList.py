5f0b88c823478fe8db5dd0bd625f274e76410e2f
thinh2904 <72325160+thinh2904@users.noreply.github.com>
2020-11-10 17:51:58 +0700

    Add files via upload

---
+++ BTList.py
@@ -0,0 +1,9 @@
+x=[]
+x.append(18)
+x.append("Huế")
+y=[12,45,-1,-0.56,"abc"]
+x=x+y
+print("Các giá trị của x:",x)
+print("Phần tử đầu tiên:",x[1],",","Phần tử cuối cùng:",x[-1])
+print("Giá trị của phần tử đầu tiên đến phần tử thứ 5 của x",x[:5])
+print("Giá trị của hần từ thứ 3 đến cuối cùng của x",x[3:])
